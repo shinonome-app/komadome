eb1b234252d643eeace365720d2092e8f3600eaf
takahashim <takahashimm@gmail.com>
2022-04-17 20:23:05 +0900

    WIP

--- app/controllers/index_pages_controller.rb
+++ app/controllers/index_pages_controller.rb
@@ -1,41 +1,112 @@
 # frozen_string_literal: true
 
 class IndexPagesController < ApplicationController
-  ROMA2KANA = {a: 'アイウエオ',
-               ka: 'カキクケコ',
-               sa: 'サシスセソ',
-               ta: 'タチツテト',
-               na: 'ナニヌネノ',
-               ha: 'ハヒフヘホ',
-               ma: 'マミムメモ',
-               ya: 'ヤユヨ',
-               ra: 'ラリルレロ',
-               wa: 'ワヲン',
-               zz: '他'
+  include Pagy::Backend
+
+  ROMA2KANA = {a: 'あいうえお',
+               ka: 'かきくけこ',
+               sa: 'さしすせそ',
+               ta: 'たちつてと',
+               na: 'なにぬねの',
+               ha: 'はひふへほ',
+               ma: 'まみむめも',
+               ya: 'やゆよ',
+               ra: 'らりるれろ',
+               wa: 'わをん',
+               zz: ''
               }
 
   def index_top
+  end
+
+  def index_all
+  end
+
+  def whatsnew
+    @pagy, @works = pagy(Work.order(:started_on).all, items: 50)
+  end
+
+  def whatsnew_year
+    @year, @page = params[:year_page].split('_')
+    logger.info("year:#{@year},#{params.inspect}")
+    @pagy, @works = pagy(Work.with_year_and_status(@year, 1).order(:started_on).all, items: 80, page: @page)
   end
 
   def person_index
     @kana_all = roma2kana(params[:id].to_sym).chars
     @kana = @kana_all[0]
 
-    person1 = Person.where('last_name_kana like ?', 'あ%')
-    person2 = Person.where('last_name_kana like ?', 'い%')
-    person3 = Person.where('last_name_kana like ?', 'う%')
-    person4 = Person.where('last_name_kana like ?', 'え%')
-    person5 = Person.where('last_name_kana like ?', 'お%')
-    @authors = [person1, person2, person3, person4, person5]
+    @authors = []
+    if @kana_all.empty?
+      @authors << Person.where('sortkey !~ ?', '^[あいうえおか-もやゆよら-ろわをんアイウエオカ-モヤユヨラ-ロワヲンヴ]')
+    else
+      @kana_all.each do |kana|
+        @authors << Person.where('sortkey like ?', "#{kana}%")
+      end
+    end
+  end
+
+  def person_all_index
+    @kana_all = roma2kana(params[:id].to_sym).chars
+    @kana = @kana_all[0]
+
+    @authors = []
+    if @kana_all.empty?
+      @authors << Person.where('sortkey !~ ?', '^[あいうえおか-もやゆよら-ろわをんアイウエオカ-モヤユヨラ-ロワヲンヴ]')
+    else
+      @kana_all.each do |kana|
+        @authors << Person.where('sortkey like ?', "#{kana}%")
+      end
+    end
+  end
+
+  def person_all
+    @authors = {}
+    ROMA2KANA.values.each do |value|
+      if value.empty?
+        @authors['その他'] = Person.where('sortkey !~ ?', '^[あいうえおか-もやゆよら-ろわをんアイウエオカ-モヤユヨラ-ロワヲンヴ]')
+      else
+        value.chars.each do |kana|
+          @authors[kana] = Person.where('sortkey like ?', "#{kana}%")
+        end
+      end
+    end
   end
 
   def person_show
     @author = Person.find(params[:id])
   end
 
+  def list_person_all
+    send_zip_file('list_person_all.zip')
+  end
+
+  def list_person_all_extended
+    send_zip_file('list_person_all_extended.zip')
+  end
+
+  def list_person_all_utf8
+    send_zip_file('list_person_all_utf8.zip')
+  end
+
+  def list_person_all_extended_utf8
+    send_zip_file('list_person_all_extended_utf8.zip')
+  end
+
+  def list_inp_show
+    @id, @page = params[:id_page].split('_')
+    @author = Person.find(@id)
+  end
+
   private
 
   def roma2kana(roma_id)
     ROMA2KANA[roma_id]
+  end
+
+  def send_zip_file(filename)
+    path = Rails.root.join('data', 'csv_zip', filename)
+    stat = File::stat(path)
+    send_file(path, filename: filename, length: stat.size)
   end
 end
