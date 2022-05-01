b038edbb1167d318cc78fc6bc4bb8c31c681b8f4
takahashim <takahashimm@gmail.com>
2022-05-01 18:29:51 +0900

    Fix top page

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class TopController < ApplicationController
-  def index; end
+  def index
+    @new_works_published_on = Work.published.order(started_on: :desc).first.started_on
+    @new_works = Work.published.where(started_on: @new_works_published_on)
+  end
 end
