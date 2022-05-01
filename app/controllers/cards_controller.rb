45afe4096d1d06954b80bc7ac8dfd62976d2ebcc
takahashim <takahashimm@gmail.com>
2022-05-02 00:00:19 +0900

    Add link.js replacement

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,5 +3,9 @@
 class CardsController < ApplicationController
   def show
     @work = Work.joins(:work_people).where('works.id = ? AND work_people.person_id = ?', params[:card_id].to_i, params[:person_id].to_i).first
+    @booklog_url = "http://booklog.jp/item/7/#{@work.id}"
+    @voyger_url = "http://aozora.binb.jp/reader/main.html?cid=#{@work.id}"
+    @airzoshi_url = "https://www.satokazzz.com/airzoshi/reader.php?action=aozora&id=#{@work.id}"
+    @rodoku_url = "https://www.google.co.jp/search?hl=ja&source=hp&q=青空文庫+朗読+#{@work.title}"
   end
 end
