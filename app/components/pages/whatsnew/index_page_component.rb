c033fb51d8cdb759eae9817a75ddc3de3250f239
takahashim <takahashimm@gmail.com>
2022-05-04 20:14:00 +0900

    refactor

--- app/components/pages/whatsnew/index_page_component.rb
+++ app/components/pages/whatsnew/index_page_component.rb
@@ -3,13 +3,16 @@
 module Pages
   module Whatsnew
     class IndexPageComponent < ViewComponent::Base
-      attr_reader :pagy, :works, :date
+      FIRST_YEAR = 2001
+
+      attr_reader :pagy, :works, :date, :prev_year
 
       def initialize(pagy:, works:, date:)
         super
         @pagy = pagy
         @works = works
         @data = date
+        @prev_year = Time.zone.parse(date).year - 1
       end
     end
   end
