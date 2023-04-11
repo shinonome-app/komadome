6bcba6ad7b514d66ef11065011b80a85fba24ef2
takahashim <takahashimm@gmail.com>
2023-04-11 13:32:23 +0900

    disable lograge

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  config.lograge.enabled = true
+  config.lograge.enabled = false
 
   config.lograge.formatter = Lograge::Formatters::Json.new
   config.colorize_logging = false
-  config.lograge.logger = ActiveSupport::Logger.new(Rails.root.join('log', "#{Rails.env}.log"))
+  config.lograge.logger = ActiveSupport::Logger.new(Rails.root.join('log', "json-#{Rails.env}.log"))
   config.lograge.custom_options = lambda do |event|
     { :ddsource => 'ruby',
       :params => event.payload[:params].reject { |k| %w[controller action].include? k } }
