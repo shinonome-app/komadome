f99c278d0d7de506480a1ba23cedf0278b412c80
takahashim <takahashimm@gmail.com>
2025-07-08 01:01:09 +0900

    rubocop

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
-  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = { exclude: ->(request) { request.path == '/up' } }
 
   config.hosts << 'job2.aozora-renewal.cloud'
 end
