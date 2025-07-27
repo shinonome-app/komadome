9f35395a021300f3fe8d1211939b54f86baae3b7
takahashim <takahashimm@gmail.com>
2025-07-27 13:24:08 +0900

    WIP

--- config/cron-build.sh
+++ config/cron-build.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -e
 
-# Set required environment variables
-export PATH=/usr/local/bundle/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-export BUNDLE_PATH=/usr/local/bundle
-export BUNDLE_APP_CONFIG=/usr/local/bundle
-export GEM_HOME=/usr/local/bundle
-export RAILS_ENV=production
-export RAILS_LOG_LEVEL=warn
+echo "Starting cron build at $(date)"
 
-# Change to app directory
+# Execute rails runner directly to bypass bundle exec
 cd /myapp
+/usr/local/bin/ruby /myapp/bin/rails runner "
+  puts 'Starting build from Rails runner...'
+  Rake.application.load_rakefile
+  Rake::Task['build:all_and_rsync'].invoke
+  puts 'Build completed.'
+" RAILS_ENV=production
 
-# Execute the build task
-bundle exec rake build:all_and_rsync
+echo "Build completed at $(date)"
